d8fc019301be43abe15488192a81ee360789c77f
Diego Lottermann <diegolottermann@Diegos-MacBook-Air.local>
2019-05-06 21:53:52 -0300

    Add styles and new components

--- Udacity/react-native/UdaciFitness/App.js
+++ Udacity/react-native/UdaciFitness/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <AddEntry />
+      <Provider store={createStore(reducer)}>
+      <View style={{flex:1}}>
+        <History />
       </View>
+    </Provider>
     )
   }
 }
--- Udacity/react-native/UdaciFitness/utils/helpers.js
+++ Udacity/react-native/UdaciFitness/utils/helpers.js
@@ -1,8 +1,8 @@
 // utils/helpers.js
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { black } from './colors'
+import { red, orange, blue, lightPurp, pink, white } from './colors'
 
 export function isBetween (num, x, y) {
     if (num >= x && num <= y) {
@@ -47,6 +47,19 @@ export function isBetween (num, x, y) {
   }
 
 
+  const styles = StyleSheet.create({
+    iconContainer: {
+      padding: 5,
+      borderRadius: 8,
+      width: 50,
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 20
+    },
+  })
+
+
   export function getMetricMetaInfo (metric) {
     const info = {
       run: {
@@ -57,10 +70,10 @@ export function isBetween (num, x, y) {
         type: 'steppers',
         getIcon() {
           return (
-            <View>
+            <View style={[styles.iconContainer, {backgroundColor: red}]}>
               <MaterialIcons
                 name='directions-run'
-                color={black}
+                color={white}
                 size={35}
               />
             </View>
@@ -75,10 +88,10 @@ export function isBetween (num, x, y) {
         type: 'steppers',
         getIcon() {
           return (
-            <View>
+            <View style={[styles.iconContainer, {backgroundColor: orange}]}>
               <MaterialCommunityIcons
                 name='bike'
-                color={black}
+                color={white}
                 size={32}
               />
             </View>
@@ -93,10 +106,10 @@ export function isBetween (num, x, y) {
         type: 'steppers',
         getIcon() {
           return (
-            <View>
+            <View style={[styles.iconContainer, {backgroundColor: blue}]}>
               <MaterialCommunityIcons
                 name='swim'
-                color={black}
+                color={white}
                 size={35}
               />
             </View>
@@ -111,10 +124,10 @@ export function isBetween (num, x, y) {
         type: 'slider',
         getIcon() {
           return (
-            <View>
+            <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
               <FontAwesome
                 name='bed'
-                color={black}
+                color={white}
                 size={30}
               />
             </View>
@@ -129,10 +142,10 @@ export function isBetween (num, x, y) {
         type: 'slider',
         getIcon() {
           return (
-            <View>
+            <View style={[styles.iconContainer, {backgroundColor: pink}]}>
               <MaterialCommunityIcons
                 name='food'
-                color={black}
+                color={white}
                 size={35}
               />
             </View>
@@ -144,4 +157,10 @@ export function isBetween (num, x, y) {
     return typeof metric === 'undefined'
       ? info
       : info[metric]
+  }
+
+  export function getDailyReminderValue () {
+    return {
+      today: "👋 Don't forget to log your data today!"
+    }
   }
